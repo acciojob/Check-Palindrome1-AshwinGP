95818d897dc7135a2886d31cdb6a668b65e9d635
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-14 18:39:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 
 function palindrome(str){
   var ct=0;
+	
 	for(var i=0;i<str.length;i++){
 		
-       if(str[i]===str[str.length-i]){
+       if(str[i]==str[str.length-i-1]){
 		    ct++;
-		     
-	   }
+		     }
 	}
     
   if(ct==str.length){
 		return true;
 	}else{
-		return false	
+		return false;
   }
 	
 }
