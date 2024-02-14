fab850bc25404527ee945774da92867e94d790ed
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-14 17:20:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
-
+  var ct=0;
+	for(var i=0;i<str.length;i++){
+		
+       if(str[i]===str[str.length-i]){
+		    ct++;
+		     
+	   }
+	}
+    
+  if(ct==str.length){
+		return true;
+	}else{
+		return false	
+  }
+	
 }
 module.exports = palindrome
